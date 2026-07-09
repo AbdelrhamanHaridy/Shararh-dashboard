6cf6b4bde70331efeb46c17607450f3293bf1061
A13DO <abdoalaa2146@gmail.com>
2026-07-09 15:51:11 +0300

    shared child primng table added

--- src/app/dashboard/pages/home/home.ts
+++ src/app/dashboard/pages/home/home.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 import { SharedKpiCard } from '../../shared/components/shared-kpi-card/shared-kpi-card';
+import { ChildTableComponent } from '../../shared/components/child-table/child-table.component';
+import { SliderModule } from 'primeng/slider';
+
 
 @Component({
   selector: 'app-home',
-  imports: [SharedKpiCard],
+  imports: [SharedKpiCard, ChildTableComponent, SliderModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  sessionColumns: any[] = [
+    { field: 'player', header: 'اسم الموظف' },
+    { field: 'sport', header: 'الدور' },
+    { field: 'date', header: 'البدء / الانتهاء' },
+    { field: 'duration', header: 'النقاط' },
+    { field: 'status', header: 'الحاله' },
+  ];
+
+  sessions: any[] = [
+    {
+      player: 'Omar Hassan',
+      sport: 'Football',
+      date: '25 Jan 2026',
+      duration: '60 min',
+      status: 'Completed',
+    },
+    {
+      player: 'Sara Ahmed',
+      sport: 'Football',
+      date: '22 Jan 2026',
+      duration: '45 min',
+      status: 'Completed',
+    },
+    {
+      player: 'Ali Youssef',
+      sport: 'Football',
+      date: '20 Jan 2026',
+      duration: '60 min',
+      status: 'Cancelled',
+    },
+    {
+      player: 'Layla Khaled',
+      sport: 'Football',
+      date: '18 Jan 2026',
+      duration: '90 min',
+      status: 'Completed',
+    },
+
+  ];
+}
---
+++ src/app/dashboard/shared/components/child-table/child-table.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-child-table',
+  imports: [CommonModule, FormsModule, TableModule, ButtonModule],
+  templateUrl: './child-table.component.html',
+  styleUrl: './child-table.component.scss',
+})
+export class ChildTableComponent {
+  @Input() tableData: any[] = [];
+  @Input() columns: any[] = [];
+  @Input() itemLabel: string = 'items';
+  @Input() title?: string;
+  @Input() showAllButton: boolean = false;
+  @Input() showAllButtonLabel: string = 'Show All';
+
+  @Output() actionClick: EventEmitter<{ action: string; row: any }> =
+    new EventEmitter();
+  @Output() showAll: EventEmitter<void> = new EventEmitter();
+
+  onActionClick(action: string, row: any) {
+    this.actionClick.emit({ action, row });
+  }
+
+  onShowAll() {
+    this.showAll.emit();
+  }
+}
