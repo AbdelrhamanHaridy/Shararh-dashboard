a6e6814499fc563c0e6fb686cc958521ab577dc8
A13DO <abdoalaa2146@gmail.com>
2026-07-13 00:12:21 +0300

    feat: enhance frequently asked questions page with new layout, header, and accordion functionality

--- src/app/dashboard/pages/frequently-asked-questions/frequently-asked-questions.ts
+++ src/app/dashboard/pages/frequently-asked-questions/frequently-asked-questions.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
+import { PageHeaderComponent } from '../../shared/components/page-header/page-header.component';
+import { SharedKpiCard } from '../../shared/components/shared-kpi-card/shared-kpi-card';
+import { SharedTextInputComponent } from '../../shared/components/shared-text-input/shared-text-input.component';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-frequently-asked-questions',
-  imports: [AccordionModule],
+  imports: [AccordionModule, CommonModule, PageHeaderComponent, SharedKpiCard],
   templateUrl: './frequently-asked-questions.html',
   styleUrl: './frequently-asked-questions.scss',
 })
 export class FrequentlyAskedQuestions {
-
+  home: MenuItem = { label: 'لوحة التحكم', routerLink: '/' };
+  breadcrumbItems: MenuItem[] = [
+    { label: 'الاسئله الشائعه', routerLink: '/frequently-asked-questions' },
+  ];
 }
