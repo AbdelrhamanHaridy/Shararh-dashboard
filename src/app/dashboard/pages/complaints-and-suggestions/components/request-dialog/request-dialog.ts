ddf0b5cfc8913bd3d31c3135b7295077f1756982
A13DO <abdoalaa2146@gmail.com>
2026-08-01 07:16:48 +0300

    feat: implement complaints and suggestions management
    - Add styles for tiered menu in complaints and suggestions page.
    - Refactor ComplaintsAndSuggestions component to extend BaseComponent and implement OnInit.
    - Introduce reactive form for handling customer requests with validation.
    - Add methods for fetching, creating, updating, and deleting customer requests.
    - Implement a dialog for editing customer requests with a dedicated EditRequestDialog component.
    - Create models for customer requests and their associated types, statuses, and categories.
    - Develop a service for managing API interactions related to customer requests.
    - Add context menu for request actions (edit, change status, delete).
    - Implement search functionality for filtering customer requests.

---
+++ src/app/dashboard/pages/complaints-and-suggestions/components/request-dialog/request-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-dialog',
+  imports: [],
+  templateUrl: './request-dialog.html',
+  styleUrl: './request-dialog.scss',
+})
+export class RequestDialog {
+
+}
